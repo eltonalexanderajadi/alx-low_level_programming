dc3e29b847e3ca64873b01a318dd4182c474f04e
eltonalexanderajadi <eltonalexander16@gmail.com>
2022-09-16 16:30:36 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,23 @@
+#include "math.h"
 #include "main.h"
 
+/**
+ * power - exponents
+ * @base: base
+ * @exp: exponent
+ * Return: result (int)
+ */
+
+int  power(int base, int exp)
+{
+int i, num;
+
+num = 1;
+for (i = 0; i < exp; ++i)
+num *= base;
+
+return (num);
+}
 /**
  * print_number - prints an integer
  * @n: number to print
